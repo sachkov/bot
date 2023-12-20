b6fb0ae22fa91ef2a5937897fc5409f4eb7071a6
Sachkov <sachkov@live.com>
2023-12-19 18:13:36 -0800

    users table fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('second_name');
-            $table->string('email');
+            $table->string('first_name')->nullable();
+            $table->string('second_name')->nullable();
+            $table->string('email')->nullable();
             $table->bigInteger('telegram_id')->unique();
             $table->string('username')->unique();
             $table->timestamps();
