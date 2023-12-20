b958c92b095b1e52f4db3e4bb81f60502094467a
Sachkov <sachkov@live.com>
2023-12-19 20:21:49 -0800

    add responses

--- app/Providers/TelegramResponseProvider.php
+++ app/Providers/TelegramResponseProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Library\Enum\StateEnum;
 use App\Library\Telegram\Responses\DefaultResponse;
+use App\Library\Telegram\Responses\Pray\AddPrayDescrResponse;
+use App\Library\Telegram\Responses\Pray\AddPrayLengthResponse;
 use Illuminate\Support\ServiceProvider;
 
 class TelegramResponseProvider extends ServiceProvider
@@ -11,6 +13,8 @@ class TelegramResponseProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(StateEnum::DEFAULT->value, DefaultResponse::class);
+        $this->app->bind(StateEnum::ADD_PRAY_DESCR->value, AddPrayDescrResponse::class);
+        $this->app->bind(StateEnum::ADD_PRAY_TIME->value, AddPrayLengthResponse::class);
     }
 
     public function boot()
